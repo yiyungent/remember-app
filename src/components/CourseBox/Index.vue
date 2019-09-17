988b3fd6e62df1c1e564c53248dc7db9cb9cb0fa
yiyun <yiyungent@gmail.com>
2019-09-17 16:47:11 +0800

    update

--- src/components/CourseBox/Index.vue
+++ src/components/CourseBox/Index.vue
@@ -1,41 +1,79 @@
 <template>
   <v-app id="inspire">
-    <div>
-      <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
-    </div>
-    <div>
-      <v-tabs fixed-tabs>
-        <v-tab>Item One</v-tab>
-        <v-tab>Item Two</v-tab>
-        <v-tab>Item Three</v-tab>
-      </v-tabs>
-    </div>
-    <div>
+    <v-content id="content">
       <v-container fluid>
+        <v-row align="start" justify="center">
+          <v-col style="padding:0;" md="8">
+            <d-player :options="playerOptions" ref="player"></d-player>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-tabs fixed-tabs>
+              <v-tab>简介</v-tab>
+              <v-tab>评论</v-tab>
+              <v-tab>测试</v-tab>
+            </v-tabs>
+          </v-col>
+        </v-row>
         <v-row>
           <v-col cols="12" xs="12">
             <div>Top western road trips</div>
             <span class="grey--text subtitle-1">1,000 miles of wonder</span>
           </v-col>
         </v-row>
-        <v-divider class="mx-4"></v-divider>
-        <!-- start 选集 -->
-        <div>选集</div>
-        <!-- end 选集 -->
-        <v-divider class="mx-4"></v-divider>
+        <v-row>
+          <v-col>
+            <v-divider class="mx-4"></v-divider>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <!-- start 选集 -->
+            <div>选集</div>
+            <!-- end 选集 -->
+          </v-col>
+        </v-row>
       </v-container>
-    </div>
+    </v-content>
   </v-app>
 </template>
 <script>
+import VueDPlayer from "vue-dplayer";
+import "vue-dplayer/dist/vue-dplayer.css";
+
 export default {
   name: "CourseBox",
   data() {
     return {
-      show: false
+      show: false,
+      playerOptions: {
+        video: {
+          url: "http://static.smartisanos.cn/common/video/t1-ui.mp4",
+          pic:
+            "http://static.smartisanos.cn/pr/img/video/video_03_cc87ce5bdb.jpg"
+        },
+        autoplay: false,
+        contextmenu: [
+          {
+            text: "GitHub",
+            link: "https://github.com/MoePlayer/vue-dplayer"
+          }
+        ]
+      },
+      player: null
     };
+  },
+  components: {
+    "d-player": VueDPlayer
+  },
+  mounted() {
+    this.player = this.$refs.player.dp;
   }
 };
 </script>
 <style lang="less" scoped>
+.container {
+    padding: 0;
+}
 </style>
