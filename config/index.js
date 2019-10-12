ed63e8657d3fd567107b54316131bc3a7ecd9293
yiyun <yiyungent@gmail.com>
2019-10-12 23:01:20 +0800

    removed: proxyTable

--- config/index.js
+++ config/index.js
@@ -11,15 +11,15 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     // proxyTable 只能在开发环境下用
-    proxyTable: {
-      '/api': {
-        target: 'http://localhost:7784/api', // 后台api访问地址
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '' //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
-        }
-      }
-    },
+    // proxyTable: {
+    //   '/api': {
+    //     target: 'http://localhost:7784/api', // 后台api访问地址
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       '^/api': '' //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+    //     }
+    //   }
+    // },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
