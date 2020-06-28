b73de03493408a5cabe5e6959f0d6da832b56b03
yiyun <yiyungent@126.com>
2020-06-28 22:16:37 +0800

    ItemList提取出CardItem

---
+++ vue/src/components/Home/CardItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card :to="{ name: 'ArticleBox', params: { id: item.article.id } }" class="mx-auto">
+    <v-img
+      class="white--text"
+      height="100px"
+      :src="item.article.picUrl"
+      :lazy-src="item.article.picUrl"
+    >
+      <v-card-title class="card-title">
+        <v-icon x-small color="white">mdi-heart</v-icon>
+        <span>{{item.favNum}}</span>
+      </v-card-title>
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-row>
+      </template>
+    </v-img>
+    <v-card-text style="height:32px;" class="card-text">{{item.article.title | subStrPretty(18)}}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+      default: function() {
+        return {
+          article: { id: 1, title: "标题", picUrl: "" },
+          favNum: 123
+        };
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+// start 文章卡片
+.card-title {
+  font-size: 12px;
+  padding-top: 76px;
+  height: 10px;
+}
+.card-text {
+  height: 50px;
+  padding: 4px;
+}
+// end 文章卡片
+</style>
--- vue/src/components/Home/ItemList.vue
+++ vue/src/components/Home/ItemList.vue
@@ -10,28 +10,7 @@
     <v-row>
       <template v-for="item in items">
         <v-col cols="6" sm="6" md="3" style="padding: 6px;" :key="item.article.id">
-          <v-card :to="{ name: 'ArticleBox', params: { id: item.article.id } }" class="mx-auto">
-            <v-img
-              class="white--text"
-              height="100px"
-              :src="item.article.picUrl"
-              :lazy-src="item.article.picUrl"
-            >
-              <v-card-title class="card-title">
-                <v-icon x-small color="white">mdi-heart</v-icon>
-                <span>{{item.favNum}}</span>
-              </v-card-title>
-              <template v-slot:placeholder>
-                <v-row class="fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                </v-row>
-              </template>
-            </v-img>
-            <v-card-text
-              style="height:32px;"
-              class="card-text"
-            >{{item.article.title | subStrPretty(18)}}</v-card-text>
-          </v-card>
+          <card-item :item="item"></card-item>
         </v-col>
       </template>
     </v-row>
@@ -39,7 +18,12 @@
 </template>
 
 <script>
+import CardItem from "./CardItem";
+
 export default {
+  components: {
+    CardItem
+  },
   props: {
     items: {
       type: Array,
@@ -47,7 +31,7 @@ export default {
       default: function() {
         return [
           {
-            article: { id: 1, title: "热门1", picUrl: "" },
+            article: { id: 1, title: "标题1", picUrl: "" },
             favNum: 123
           }
         ];
@@ -58,15 +42,4 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-// start 文章卡片
-.card-title {
-  font-size: 1rem;
-  padding-top: 82px;
-  height: 10px;
-}
-.card-text {
-  height: 50px;
-  padding: 4px;
-}
-// end 文章卡片
 </style>
