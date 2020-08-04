67babe51360c7d7de1866badc22acc05a3b54cd6
yiyun <yiyungent@126.com>
2020-08-05 01:59:00 +0800

    feat: update vue-sim-captcha v0.1.1

--- vue/src/components/Account/Login.vue
+++ vue/src/components/Account/Login.vue
@@ -79,8 +79,9 @@ export default {
       captcha: {
         appId: "132132",
         source: {
-          reqVCodeImgUrl: "https://captcha.moeci.com/api/vCode/VCodeImg",
-          reqVCodeCheckUrl: "https://captcha.moeci.com/api/vCode/VCodeCheck"
+          baseUrl: "https://captcha.moeci.com",
+          imgUrl: "/api/vCode/VCodeImg",
+          checkUrl: "/api/vCode/VCodeCheck"
         },
         showCaptcha: false,
         ticket: "",
